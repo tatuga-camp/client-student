9c723436f52edf3bfaf468785b018645a10f5319
permlap-phola <permlap@tatugacamp.com>
2023-12-16 16:28:17 +0700

    Build time error: add missing dependency

--- pages/classroom/index.js
+++ pages/classroom/index.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
+import Swal from "sweetalert2";
 
 function Index() {
   const router = useRouter();
