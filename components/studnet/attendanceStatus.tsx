c1999b47b2b4497fca7812c73c624521b4aeb022
permlap-phola <permlap@tatugacamp.com>
2024-01-12 17:58:56 +0700

    Fix error on typeScript (https://github.com/tatuga-camp/client-student/pull/3)

--- components/studnet/attendanceStatus.tsx
+++ components/studnet/attendanceStatus.tsx
@@ -1,158 +1,147 @@
 import { UseQueryResult } from "@tanstack/react-query";
 import React from "react";
 import { ResponseGetAttendancesService } from "../../service/student/attendance";
-import { BiErrorCircle, BiHappyBeaming, BiRun } from "react-icons/bi";
-import { MdCardTravel, MdOutlineMoodBad, MdOutlineSick } from "react-icons/md";
-import {Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
-import { Pie } from 'react-chartjs-2';
+import { Pie } from "react-chartjs-2";
 import ChartDataLabels from "chartjs-plugin-datalabels";
+import type { ChartData, ChartOptions } from "chart.js";
 
 interface AttendanceStatus {
   attendances: UseQueryResult<ResponseGetAttendancesService, Error>;
 }
 function AttendanceStatus({ attendances }: AttendanceStatus) {
-  //!Bug : 
-  const data = {
-    labels: false,
+  const data: ChartData<"pie"> = {
     datasets: [
       {
-        label: false,
         data: [
-          attendances?.data?.statistics?.number?.present || 0, 
+          attendances?.data?.statistics?.number?.present || 0,
           attendances?.data?.statistics?.number?.holiday || 0,
           attendances?.data?.statistics?.number?.sick || 0,
-          attendances?.data?.statistics?.number?.absent || 0,  
-          attendances?.data?.statistics?.number?.late || 0, 
-          ],
+          attendances?.data?.statistics?.number?.absent || 0,
+          attendances?.data?.statistics?.number?.late || 0,
+        ],
         backgroundColor: [
           //green yellow blue red orange
-          'rgba(0, 180, 81, 1)' ,
-          'rgba(237, 186, 2, 1)',
-          'rgba(44, 124, 209, 1)',
-          'rgba(237, 2, 2, 1)',
-          'rgba(245, 94, 0, 1)',
+          "rgba(0, 180, 81, 1)",
+          "rgba(237, 186, 2, 1)",
+          "rgba(44, 124, 209, 1)",
+          "rgba(237, 2, 2, 1)",
+          "rgba(245, 94, 0, 1)",
         ],
-        
       },
     ],
   };
-  const option2 = {
+  const option = {
     tooltips: {
       enabled: false,
     },
     plugins: {
       datalabels: {
-        formatter: (value, ctx) => {
+        formatter: (value: any, ctx: any) => {
           let sum = 0;
           let dataArr = ctx.chart.data.datasets[0].data;
-  
-          dataArr.forEach(data => {
+
+          dataArr.forEach((data: any) => {
             sum += data;
           });
-  
-          
-          let percentage = value > 0 ? ((value * 100) / sum).toFixed() + "%" : "";
-  
+
+          let percentage =
+            value > 0 ? ((value * 100) / sum).toFixed() + "%" : "";
+
           return percentage;
         },
-        color: '#fff',
+        color: "#fff",
       },
     },
   };
-  
-  
-  
+
   return (
-    <section className="w-full h-full flex  justify-center bg-white rounded-3xl">   
+    <section className="w-full h-full flex  justify-center bg-white rounded-3xl">
       <ul className="grid pl-0 grid-cols-1 gap-1 w-full h-full py-5 max-w-3xl md:w-10/12  place-items-center">
         <div className="w-full flex justify-start flex-col items-center font-Kanit ">
           <div className="flex">
-                {/* Chart */}
-              <div className="w-[150px] h-[150px]">
-              <Pie options={option2} data={data} />
-              </div>
+            {/* Chart */}
+            <div className="w-[150px] h-[150px]">
+              <Pie options={option} data={data} />
+            </div>
 
-              {/* Info stats */}
-              <div className="-mt-5 ml-2 flex flex-col text-[#2C7CD1]">
-                  <h2 className="font-bold text-xl mt-2">ข้อมูลการมาเรียน</h2>
-                  <span className="text-sm"> 
-                    เปอเซ็นต์การเข้าเรียน 
-                    {attendances?.data?.statistics?.percent?.present?.toFixed(2)}%
-                  </span>
-                  <span className="text-sm"> 
-                    จำนวนคาบเรียน 
-                    {attendances?.data?.statistics?.sum  || 0} คาบ
-                  </span>
+            {/* Info stats */}
+            <div className="-mt-5 ml-2 flex flex-col text-[#2C7CD1]">
+              <h2 className="font-bold text-xl mt-2">ข้อมูลการมาเรียน</h2>
+              <span className="text-sm">
+                เปอเซ็นต์การเข้าเรียน
+                {attendances?.data?.statistics?.percent?.present?.toFixed(2)}%
+              </span>
+              <span className="text-sm">
+                จำนวนคาบเรียน
+                {attendances?.data?.statistics?.sum || 0} คาบ
+              </span>
 
-                  <ul className="ml-3 mt-2 text-sm pr-6">
-                    <li className="flex gap-[0.4rem] justify-between">
-                      <div className="flex gap-2 items-center">
-                        <div className="w-[15px] h-[15px] rounded-full bg-[#00B451]"></div>
-                        <p>มาเรียน </p>
-                      </div>
-                      <span>
-                        {attendances?.data?.statistics?.number?.present  || 0}  
-                        <span className="ml-2">ครั้ง</span>
-                      </span>
-                    </li>
-                    <li className="flex gap-[0.4rem] justify-between">
-                      <div className="flex gap-2 items-center">
-                        <div className="w-[15px] h-[15px] rounded-full bg-[#EDBA02]"></div>
-                        <p>ลา </p>
-                      </div>
-                      <span>
-                      {attendances?.data?.statistics?.number?.holiday  || 0}  
-                      <span className="ml-2">ครั้ง</span>
-                      </span>
-                    </li>
-                    <li className="flex gap-[0.4rem] justify-between">
-                      <div className="flex gap-2 items-center">
-                        <div className="w-[15px] h-[15px] rounded-full bg-[#2C7CD1]"></div>
-                        <p>ป่วย </p>
-                      </div>
-                      <span>
-                      {attendances?.data?.statistics?.number?.sick  || 0}  
-                      <span className="ml-2">ครั้ง</span>
-                      </span>
-                    </li>
-                    <li className="flex gap-[0.4rem] justify-between">
-                      <div className="flex gap-2 items-center">
-                        <div className="w-[15px] h-[15px] rounded-full bg-[#ED0202]"></div>
-                        <p>ขาดเรียน </p>
-                      </div>
-                      <span>
-                      {attendances?.data?.statistics?.number?.absent  || 0}
-                      <span className="ml-2">ครั้ง</span>
-                      </span>
-                    </li>
-                    <li className="flex gap-[0.4rem] justify-between">
-                      <div className="flex gap-2 items-center">
-                        <div className="w-[15px] h-[15px] rounded-full bg-[#F55E00]"></div>
-                        <p>มาสาย </p>
-                      </div>
-                      <span>
-                      {attendances?.data?.statistics?.number?.late || 0}
-                      <span className="ml-2">ครั้ง</span>
-                      </span>
-                    </li>
-                    
-                  </ul>
-              </div>
+              <ul className="ml-3 mt-2 text-sm pr-6">
+                <li className="flex gap-[0.4rem] justify-between">
+                  <div className="flex gap-2 items-center">
+                    <div className="w-[15px] h-[15px] rounded-full bg-[#00B451]"></div>
+                    <p>มาเรียน </p>
+                  </div>
+                  <span>
+                    {attendances?.data?.statistics?.number?.present || 0}
+                    <span className="ml-2">ครั้ง</span>
+                  </span>
+                </li>
+                <li className="flex gap-[0.4rem] justify-between">
+                  <div className="flex gap-2 items-center">
+                    <div className="w-[15px] h-[15px] rounded-full bg-[#EDBA02]"></div>
+                    <p>ลา </p>
+                  </div>
+                  <span>
+                    {attendances?.data?.statistics?.number?.holiday || 0}
+                    <span className="ml-2">ครั้ง</span>
+                  </span>
+                </li>
+                <li className="flex gap-[0.4rem] justify-between">
+                  <div className="flex gap-2 items-center">
+                    <div className="w-[15px] h-[15px] rounded-full bg-[#2C7CD1]"></div>
+                    <p>ป่วย </p>
+                  </div>
+                  <span>
+                    {attendances?.data?.statistics?.number?.sick || 0}
+                    <span className="ml-2">ครั้ง</span>
+                  </span>
+                </li>
+                <li className="flex gap-[0.4rem] justify-between">
+                  <div className="flex gap-2 items-center">
+                    <div className="w-[15px] h-[15px] rounded-full bg-[#ED0202]"></div>
+                    <p>ขาดเรียน </p>
+                  </div>
+                  <span>
+                    {attendances?.data?.statistics?.number?.absent || 0}
+                    <span className="ml-2">ครั้ง</span>
+                  </span>
+                </li>
+                <li className="flex gap-[0.4rem] justify-between">
+                  <div className="flex gap-2 items-center">
+                    <div className="w-[15px] h-[15px] rounded-full bg-[#F55E00]"></div>
+                    <p>มาสาย </p>
+                  </div>
+                  <span>
+                    {attendances?.data?.statistics?.number?.late || 0}
+                    <span className="ml-2">ครั้ง</span>
+                  </span>
+                </li>
+              </ul>
+            </div>
           </div>
-
-        
-          
         </div>
 
         {/* Date statistic */}
         <div className="flex  items-center justify-center font-Kanit w-full md:w-full rounded-md gap-1 mt-6">
-                <div className="flex justify-center items-center rounded-lg w-[220px] h-[30px] bg-[#2C7CD1] text-white ">
-                  <span className=" font-normal text-sm">วันที่</span>
-                </div>
-                <div className="flex justify-center items-center rounded-lg w-[100px] h-[30px] bg-[#2C7CD1]  text-white">
-                  <span>สถานะ</span>
-                </div>
+          <div className="flex justify-center items-center rounded-lg w-[220px] h-[30px] bg-[#2C7CD1] text-white ">
+            <span className=" font-normal text-sm">วันที่</span>
+          </div>
+          <div className="flex justify-center items-center rounded-lg w-[100px] h-[30px] bg-[#2C7CD1]  text-white">
+            <span>สถานะ</span>
+          </div>
         </div>
 
         {attendances?.data?.students?.map((attendance) => {
@@ -214,7 +203,6 @@ function AttendanceStatus({ attendances }: AttendanceStatus) {
                 className="flex  items-center justify-center font-Kanit w-full md:w-full rounded-md gap-1"
               >
                 <div className="flex justify-center items-center rounded-lg w-[220px] h-[30px] bg-[#E8E8E8] text-[#2C7CD1]">
-                  
                   <span className=" font-normal text-sm">{formattedDate}</span>
                 </div>
                 <div className="flex justify-center items-center rounded-lg w-[100px] h-[30px] bg-[#ED0202]  text-white">
@@ -229,7 +217,6 @@ function AttendanceStatus({ attendances }: AttendanceStatus) {
                 className="flex  items-center justify-center font-Kanit w-full md:w-full rounded-md gap-1"
               >
                 <div className="flex justify-center items-center rounded-lg w-[220px] h-[30px] bg-[#E8E8E8] text-[#2C7CD1]">
-                  
                   <span className=" font-normal text-sm">{formattedDate}</span>
                 </div>
                 <div className="flex justify-center items-center rounded-lg w-[100px] h-[30px] bg-[#F55E00]  text-white">
--- components/studnet/scoreStatus.tsx
+++ components/studnet/scoreStatus.tsx
@@ -10,11 +10,11 @@ interface ScoreStatus {
 function ScoreStatus({ totalScore }: ScoreStatus) {
   const [activeScore, setActiveScore] = useState<number | null>();
 
-  const calculateAllAssignmentScore = () => {
+  const calculateAllAssignmentScore = (): string => {
     let total = 0;
 
     totalScore?.data?.assignments?.forEach((assignment) => {
-      let pureMaxScore = "0"
+      let pureMaxScore = "0";
 
       if (assignment?.assignment?.percentage) {
         pureMaxScore = assignment?.assignment.percentage.replace(/%/g, "");
@@ -23,25 +23,25 @@ function ScoreStatus({ totalScore }: ScoreStatus) {
       }
 
       total += parseFloat(pureMaxScore);
-    })
+    });
     return total.toFixed(2);
-  }
+  };
 
-  const calculateAssignmentScore = () => {
+  const calculateAssignmentScore = (): string => {
     let total = 0;
 
     totalScore?.data?.assignments?.forEach((assignment) => {
       let score = 0;
-      
-      if (assignment.studentWork){
+
+      if (assignment.studentWork) {
         score = assignment.studentWork.score as number;
       }
 
       total += score;
-    })
+    });
 
     return total.toFixed(2);
-  }
+  };
 
   return (
     <section className="w-full md:mt-5 flex font-Kanit flex-col items-center gap-5 justify-center">
@@ -54,41 +54,37 @@ function ScoreStatus({ totalScore }: ScoreStatus) {
           {/* คะแนนชิ้นงาน */}
           <div className="p-2 bg-[#00B451] rounded-[1.8rem]">
             <div className=" w-24 h-24 text-center flex-col text-white bg-[#00B451] rounded-[1.4rem] flex items-center justify-center border-solid border-2 border-white">
-                <span className="text-xs">คะแนนชิ้นงาน</span>
-                <span className="text-2xl font-Kanit font-semibold">
-                  {calculateAssignmentScore()}
-                </span>
-                <div className="w-10/12 h-[2px] bg-white"></div>
-                <span className="text-base font-semibold ">
-                  {calculateAllAssignmentScore()}
-                </span>
+              <span className="text-xs">คะแนนชิ้นงาน</span>
+              <span className="text-2xl font-Kanit font-semibold">
+                {calculateAssignmentScore()}
+              </span>
+              <div className="w-10/12 h-[2px] bg-white"></div>
+              <span className="text-base font-semibold ">
+                {calculateAllAssignmentScore()}
+              </span>
             </div>
           </div>
-            
+
           {/* คะแนนพิเศษ */}
           <div className="p-2 bg-[#EDBA02] rounded-[1.8rem]">
-              <div className=" w-24 h-24 text-center flex-col text-white bg-[#EDBA02] rounded-[1.4rem] flex items-center justify-center border-solid border-2 border-white">
-                <span className="text-xs">คะแนนพิเศษ</span>
-                <span className="text-3xl font-Kanit font-bold">
-                  {totalScore?.data?.speicalScore.toFixed(2)}
-                </span>
-              </div>
+            <div className=" w-24 h-24 text-center flex-col text-white bg-[#EDBA02] rounded-[1.4rem] flex items-center justify-center border-solid border-2 border-white">
+              <span className="text-xs">คะแนนพิเศษ</span>
+              <span className="text-3xl font-Kanit font-bold">
+                {totalScore?.data?.speicalScore.toFixed(2)}
+              </span>
+            </div>
           </div>
 
           {/* คะแนนรวม */}
-            <div className="p-2 bg-[#9C2CD1] rounded-[1.8rem]">
-              <div className=" w-24 h-24 text-center flex-col text-white bg-[#9C2CD1] rounded-[1.4rem] flex items-center justify-center border-solid border-2 border-white">
-                  <span className="text-xs">คะแนนรวม</span>
-                  <span className="text-3xl font-Kanit font-bold">
-                            {totalScore?.data?.totalScore.toFixed(2)}
-                   </span>
-              </div>
+          <div className="p-2 bg-[#9C2CD1] rounded-[1.8rem]">
+            <div className=" w-24 h-24 text-center flex-col text-white bg-[#9C2CD1] rounded-[1.4rem] flex items-center justify-center border-solid border-2 border-white">
+              <span className="text-xs">คะแนนรวม</span>
+              <span className="text-3xl font-Kanit font-bold">
+                {totalScore?.data?.totalScore.toFixed(2)}
+              </span>
             </div>
-
-            
+          </div>
         </div>
-       
-        
       )}
       {totalScore.isLoading ? (
         <ul className="w-11/12 grid grid-cols-3 place-items-center gap-4 ">
@@ -103,24 +99,26 @@ function ScoreStatus({ totalScore }: ScoreStatus) {
         </ul>
       ) : (
         <section className="flex flex-col items-center justify-center gap-5 mb-5">
-
           {totalScore?.data?.assignments?.map((assignment, index) => {
             let score: number = 0;
             let pureMaxScore = "0";
 
             const createDate = new Date(assignment.assignment?.createAt);
             const formattedCreateDate = createDate.toLocaleDateString("th-TH", {
-                    day: "2-digit",
-                    month: "2-digit",
-                    year: "numeric",
-            });
-          
-            let deadlineDate = new Date(assignment.assignment?.deadline);
-            const formatteDeadlineDate = deadlineDate.toLocaleDateString("th-TH", {
               day: "2-digit",
               month: "2-digit",
               year: "numeric",
             });
+
+            let deadlineDate = new Date(assignment.assignment?.deadline);
+            const formatteDeadlineDate = deadlineDate.toLocaleDateString(
+              "th-TH",
+              {
+                day: "2-digit",
+                month: "2-digit",
+                year: "numeric",
+              }
+            );
 
             if (assignment.studentWork) {
               score = assignment.studentWork.score as number;
@@ -135,57 +133,50 @@ function ScoreStatus({ totalScore }: ScoreStatus) {
             }
             return (
               <li
-              key={assignment.id}
-              className="w-full no-underline hover:scale-110 transition duration-100 flex gap-5 items-center 
+                key={assignment.id}
+                className="w-full no-underline hover:scale-110 transition duration-100 flex gap-5 items-center 
               justify-center bg-white ring overflow-auto ring-[#2C7CD1] rounded-2xl p-3 "
               >
-              {/* Assignment Card*/}
-              <div className="flex justify-center items-center gap-3">
-                   {/* Left hand card */}
-                   <div className="w-full pl-2 h-28 flex flex-col justify-start  text-left ">
-                        {/* Left : Title */}
-                        <div className="pt-2 w-48 md:w-72 text-left truncate scrollbar-hide ">
-                            <span className="font-Kanit font-semibold text-xl text-[#2C7CD1]">
-                              {assignment.assignment?.title}
-                            </span>
-                        </div>
-                        {/* Left : Date Deadline */}
-                        <div className="flex flex-row text-[#F55E00] font-Kanit text-[0.6rem] gap-2">
-                            <span className=" ">
-                              วันที่: {formattedCreateDate}
-                            </span>
-                            <span className=" ">
-                              วันสิ้นสุด: {formatteDeadlineDate}
-                            </span>
-                        </div>
-                        {/* Left : Description */}
-                        <div className="pt-1 text-black font-Kanit text-sm mt-2 leading-4 w-[12.5rem] h-[3.125rem] md:w-[18.75rem] overflow-hidden"
-             dangerouslySetInnerHTML={{ __html: assignment.assignment?.description }}>
-                             
-                        </div>
-
-                   </div>
-
-                   {/* Right hand */}
-                   <div className="w-28 h-28 mr-2 flex items-center justify-center ">
-                      <div className="flex flex-col w-24 h-24 bg-[#00B451] rounded-2xl text-white font-Kanit  justify-center items-center">
-                        <span className="text-2xl font-semibold">
-                            {score.toFixed(2)}
-                        </span>
-                        <div className="w-10/12 h-[2px] bg-white"></div>
-                        <span className="text-base ">
-                            {pureMaxScore}
-                        </span>
-                      </div>
-                      
-                   </div>
-                    
+                {/* Assignment Card*/}
+                <div className="flex justify-center items-center gap-3">
+                  {/* Left hand card */}
+                  <div className="w-full pl-2 h-28 flex flex-col justify-start  text-left ">
+                    {/* Left : Title */}
+                    <div className="pt-2 w-48 md:w-72 text-left truncate scrollbar-hide ">
+                      <span className="font-Kanit font-semibold text-xl text-[#2C7CD1]">
+                        {assignment.assignment?.title}
+                      </span>
+                    </div>
+                    {/* Left : Date Deadline */}
+                    <div className="flex flex-row text-[#F55E00] font-Kanit text-[0.6rem] gap-2">
+                      <span className=" ">วันที่: {formattedCreateDate}</span>
+                      <span className=" ">
+                        วันสิ้นสุด: {formatteDeadlineDate}
+                      </span>
+                    </div>
+                    {/* Left : Description */}
+                    <div
+                      className="pt-1 text-black font-Kanit text-sm mt-2 leading-4 w-[12.5rem] h-[3.125rem] md:w-[18.75rem] overflow-hidden"
+                      dangerouslySetInnerHTML={{
+                        __html: assignment.assignment?.description,
+                      }}
+                    ></div>
                   </div>
 
+                  {/* Right hand */}
+                  <div className="w-28 h-28 mr-2 flex items-center justify-center ">
+                    <div className="flex flex-col w-24 h-24 bg-[#00B451] rounded-2xl text-white font-Kanit  justify-center items-center">
+                      <span className="text-2xl font-semibold">
+                        {score.toFixed(2)}
+                      </span>
+                      <div className="w-10/12 h-[2px] bg-white"></div>
+                      <span className="text-base ">{pureMaxScore}</span>
+                    </div>
+                  </div>
+                </div>
               </li>
             );
           })}
-          
         </section>
       )}
     </section>
